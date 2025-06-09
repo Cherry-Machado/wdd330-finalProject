cb6e27c29862347b5ba2bba566115b5d63150c69
Cherry-Machado <Cherry5467@gmail.com>
2025-06-08 23:19:37 -0300

    Creating routes.js to handle the routs

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
+const router = require('./routes');
 const app = express();
 
-app.use('/', (req, res) => {
-  res.send('Hello, World!');
-});
+app.use('/', router());
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
