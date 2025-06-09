9be83fef8a483054eba453e27d783614a0f565b0
Cherry-Machado <Cherry5467@gmail.com>
2025-06-08 23:34:39 -0300

    Setting up handlebars like view engine for this project

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = require('./routes');
-const app = express();
-
-app.use('/', router());
-
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
