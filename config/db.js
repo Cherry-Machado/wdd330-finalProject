15f6a1c4caa4ba1793bc60559c155873a03deecd
Cherry-Machado <Cherry5467@gmail.com>
2025-06-09 17:41:06 -0300

    Connecting database

---
+++ config/db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+require('dotenv').config({path: 'variables.env'});
+
+// Connect to MongoDB using the connection string from environment variables
+mongoose.connect(process.env.DATABASE_URL)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('Connection error', err));
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+const mongoose = require('mongoose');
+require('./config/db');
 const path = require('path');
 const express = require('express');
 const {engine} = require('express-handlebars');
 const router = require('./routes');
 const app = express();
+
+require('dotenv').config({path: 'variables.env'});
 
 
 app.use('/', router());
@@ -15,4 +19,4 @@ app.set('views', './views');
 //static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(5000);
+app.listen(process.env.PORT);
