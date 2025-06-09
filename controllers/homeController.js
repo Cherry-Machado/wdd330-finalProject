0defc039bf7356f514fe9b48e93de85c4b8149cf
Cherry-Machado <Cherry5467@gmail.com>
2025-06-09 03:31:54 -0300

    Updating handlebars bugs

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,9 @@
 exports.mostrarTrabajos = (req, res) => {
     res.render('home', {
+       title: 'WDD330',
        nombrePagina: 'softdevJobs',
        tagline: 'Find and post jobs for software developers',
        barra: true,
        boton: true
-    })
-}
+    });
+};
--- index.js
+++ index.js
@@ -1,23 +1,18 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
 const path = require('path');
+const express = require('express');
+const {engine} = require('express-handlebars');
 const router = require('./routes');
-
 const app = express();
 
 
 app.use('/', router());
 
 // Set up Handlebars view engine
-app.engine('handlebars', exphbs({
-        defaultLayout: 'layout'
-        //layoutsDir: __dirname + './views/layouts/'
-    })
-);
-
+app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 //static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000);
+app.listen(5000);
