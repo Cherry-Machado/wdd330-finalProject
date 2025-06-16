c4f138ee3894873dea03e01b678d7c39917e50f1
Cherry-Machado <Cherry5467@gmail.com>
2025-06-16 03:43:05 -0300

    Updating

--- public/js/app.js
+++ public/js/app.js
@@ -10,8 +10,18 @@ const skills = new Set();
 // Function to add skills to the input field    
 const agregarSkills = (e) => {
     if (e.target.tagName === 'LI') {
-        skills.add(e.target.textContent);
-        
+       if (e.target.classList.contains('activo')) {
+           skills.delete(e.target.textContent);
+           e.target.classList.remove('activo');
+           
+       } else {
+           skills.add(e.target.textContent);
+           e.target.classList.add('activo');
+           
+       }
     }
-    console.log(skills);
+    // Convert the Set to an Array and join with commas
+    const skillsArray = [...skills];
+    document.querySelector('#skills').value = skillsArray.join(',');  // Convert Set to Array and join with commas
+    console.log(skillsArray);
 }
